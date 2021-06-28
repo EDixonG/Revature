cebe380c6a363f2e224501f0a1405933a384d9cd
StevenMagnin <stevenmagnin@gmail.com>
2021-06-28 16:01:26 -0400

    added top banner and fixed number formatting on month selector

--- force-app/main/default/aura/Calendar/CalendarController.js
+++ force-app/main/default/aura/Calendar/CalendarController.js
@@ -1,9 +1,13 @@
 ({
     doInit : function(component, event, helper) {
         let currentDate = new Date();
-        component.set("v.currentMonth", currentDate.getMonth() + 1);
-        component.set("v.currentYear", currentDate.getFullYear());
+        let month = currentDate.getMonth() + 1;
+        let year = currentDate.getFullYear();
+        component.set("v.currentMonth", month);
+        component.set("v.currentYear", year);
         let getEventList = component.get("c.getEventList");
+        let getMonthName = component.get("c.getMonthName");
+        getMonthName.setParams({"monthToView": month})
 
         getEventList.setCallback(this, function(response){
             if(response.getState() === "SUCCESS"){
@@ -11,14 +15,22 @@
             }
         });
 
+        getMonthName.setCallback(this, function(response){
+            if(response.getState() === "SUCCESS"){
+                component.set("v.monthName", response.getReturnValue());
+                component.set("v.selectedYear", year);
+            }
+        });
+
         $A.enqueueAction(getEventList);
+        $A.enqueueAction(getMonthName);
     },
 
     handleCalendarButton : function(component, event, helper) {
         let monthField = component.find("monthInput");
         let yearField = component.find("yearInput");
-        let month = monthField.get("v.value");
-        let year = yearField.get("v.value");
+        let month = parseInt(monthField.get("v.value"));
+        let year = parseInt(yearField.get("v.value"));
         let updateCalendar = [month, year];
         helper.updateHelper(component, updateCalendar);
     }
