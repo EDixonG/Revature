2619d3f21fe4591fec3e51bd2f4a38f9f14f5d68
StevenMagnin <stevenmagnin@gmail.com>
2021-06-28 15:05:21 -0400

    Calendar is working

--- force-app/main/default/aura/Calendar/CalendarController.js
+++ force-app/main/default/aura/Calendar/CalendarController.js
@@ -1,5 +1,25 @@
 ({
-    myAction : function(component, event, helper) {
+    doInit : function(component, event, helper) {
+        let currentDate = new Date();
+        component.set("v.currentMonth", currentDate.getMonth() + 1);
+        component.set("v.currentYear", currentDate.getFullYear());
+        let getEventList = component.get("c.getEventList");
 
+        getEventList.setCallback(this, function(response){
+            if(response.getState() === "SUCCESS"){
+                component.set("v.eventList", response.getReturnValue());
+            }
+        });
+
+        $A.enqueueAction(getEventList);
+    },
+
+    handleCalendarButton : function(component, event, helper) {
+        let monthField = component.find("monthInput");
+        let yearField = component.find("yearInput");
+        let month = monthField.get("v.value");
+        let year = yearField.get("v.value");
+        let updateCalendar = [month, year];
+        helper.updateHelper(component, updateCalendar);
     }
 })
--- force-app/main/default/aura/Calendar/CalendarHelper.js
+++ force-app/main/default/aura/Calendar/CalendarHelper.js
@@ -1,5 +1,19 @@
 ({
-    helperMethod : function() {
+    updateHelper : function(component, date) {
+        let month = date[0];
+        let year = date[1];
+        
+        console.log(date);
 
+        let updateCalendar = component.get("c.getEventListRefresh");
+        updateCalendar.setParams({"monthToView" : month, "yearToView" : year});
+
+        updateCalendar.setCallback(this, function(response){
+            if(response.getState() === "SUCCESS"){
+                component.set("v.eventList", response.getReturnValue());
+            }
+        });
+
+        $A.enqueueAction(updateCalendar);
     }
 })
